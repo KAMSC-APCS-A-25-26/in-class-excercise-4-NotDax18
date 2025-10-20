dc508446e84d4506d8af55de846733a358df907f
NotDax18 <dax.cw.robinson@gmail.com>
2025-10-20 13:42:31 -0400

    Update ShoppingCart.java

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -2,10 +2,29 @@ import java.util.Scanner;
 
 public class ShoppingCart {
     public static void main(String[] args) {
-        // TODO: Ask the user how many items they are purchasing
-        // TODO: Use a for loop to read each item's price (double)
-        // TODO: Accumulate the total cost
-        // TODO: Print the final summary line with item count and total
-        // TODO: Use Scanner for input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many items are you purchasing? ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid integer for number of items.");
+            scanner.next();
+            System.out.print("How many items are you purchasing? ");
+        }
+        int itemCount = scanner.nextInt();
+
+        double total = 0.0;
+        for (int i = 1; i <= itemCount; i++) {
+            System.out.print("Price of item " + i + "? ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Please enter a valid price (decimal number).");
+                scanner.next();
+                System.out.print("Price of item " + i + "? ");
+            }
+            double price = scanner.nextDouble();
+            total += price;
+        }
+
+        System.out.printf("Your cart has %d item%s with a total cost of $%.2f%n",
+                itemCount, itemCount == 1 ? "" : "s", total);
     }
 }
